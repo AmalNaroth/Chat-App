262cf2088ea5922e74e1bc333919461011cba558
amalnaroth <amalzenox129@gmail.com>
2023-07-07 00:29:35 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: "Material Chat App",
+      home: const HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        brightness: Brightness.light
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const 
+    Scaffold();
+  }
+}
